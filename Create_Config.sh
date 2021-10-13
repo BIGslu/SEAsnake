2801807490551592772fa2d694bec42d09e931d5
rmsegnitz <43895313+rmsegnitz@users.noreply.github.com>
2021-10-13 14:32:20 -0700

    Update Create_Config.sh

--- Create_Config.sh
+++ Create_Config.sh
@@ -7,14 +7,14 @@
 
 samples_list="data/samples/*R1*"
 
-#echo $samples_list
 
 echo "samples:" > config/test_config.yaml
+
 spacer1=": "
 spacer2=" "
 indent="    "
 
-# List samples
+# List samples & paired reads
 for sample in $samples_list;
 do 
 sample_name1_simple="$(basename $sample)"
@@ -25,13 +25,21 @@ sample_name1_simple2=`echo "$sample_name1_simple" | cut -d'.' -f1`
 sample_name2_simple2=`echo "${sample_name1_simple2/R1/R2}"`
 
 sample_name_spaced="$indent$sample_name$spacer1$sample_name1_simple2$spacer2$sample_name2_simple2"
-echo $sample_name
+
 echo $sample_name_spaced >> config/test_config.yaml
 done
 
+# Build the rest of the config file
 echo "extension:
     .html: .html
-    _fastqc.zip: _fastqc.zip" >> config/test_config.yaml
+    _fastqc.zip: _fastqc.zip
+trim5p:
+    10
+adapter1:
+    "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC"
+adapter2:
+    "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT"
+" >> config/test_config.yaml
 
 
 
