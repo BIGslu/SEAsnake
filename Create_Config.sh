ed1d594d02f20a6ca69b0396fc34a11e4d4f8ced
rmsegnitz <43895313+rmsegnitz@users.noreply.github.com>
2021-10-13 14:12:11 -0700

    Update Create_Config.sh
    Update config creation script to handle paired samples. Lists samples base names along with corresponding R1 & R2 files.

--- Create_Config.sh
+++ Create_Config.sh
@@ -5,21 +5,27 @@
 #################################
 
 
-samples_list=(data/samples/*)
+samples_list="data/samples/*R1*"
 
+#echo $samples_list
 
 echo "samples:" > config/test_config.yaml
-spacer=": "
+spacer1=": "
+spacer2=" "
 indent="    "
 
 # List samples
 for sample in $samples_list;
 do 
-sample_name_simple="$(basename $sample)"
-sample_name_simple2=`echo "$sample_name_simple" | cut -d'.' -f1`
+sample_name1_simple="$(basename $sample)"
+sample_name=`echo "$sample_name1_simple" | grep -o '^.*_L' | sed 's/_L$//'`
 
+sample_name1_simple2=`echo "$sample_name1_simple" | cut -d'.' -f1`
 
-sample_name_spaced="$indent$sample_name_simple2$spacer$sample_name_simple2"
+sample_name2_simple2=`echo "${sample_name1_simple2/R1/R2}"`
+
+sample_name_spaced="$indent$sample_name$spacer1$sample_name1_simple2$spacer2$sample_name2_simple2"
+echo $sample_name
 echo $sample_name_spaced >> config/test_config.yaml
 done
 
