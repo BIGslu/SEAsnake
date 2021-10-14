66052326e1c774f1d431b9656508187b3da321a1
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2021-10-13 19:50:56 -0700

    Update create config
    match names to step2 workflow, add param

--- Create_Config.sh
+++ Create_Config.sh
@@ -5,17 +5,17 @@
 #################################
 
 
-samples_list="data/samples/*R1*"
+SampleList="data/*R1*"
 
 
-echo "sample_list:" > config/test_config.yaml
+echo "SampleList:" > config/test_config.yaml
 
 spacer1=": "
-spacer2=" "
-indent="    "
+spacer2=""
+indent="  "
 
 # List samples & paired reads
-for sample in $samples_list;
+for sample in $SampleList;
 do 
 sample_name1_simple="$(basename $sample)"
 sample_name=`echo "$sample_name1_simple" | grep -o '^.*_L' | sed 's/_L$//'`
@@ -26,28 +26,20 @@ sample_name2_simple2=`echo "${sample_name1_simple2/R1/R2}"`
 
 # sample_name_spaced="$indent$sample_name$spacer1$sample_name1_simple2$spacer2$sample_name2_simple2"
 echo "  " "$sample_name$spacer1" >> config/test_config.yaml
-echo "    sample: " $sample_name >> config/test_config.yaml
+echo "    sample: '"$sample_name"'" >> config/test_config.yaml
 # echo $sample_name_spaced >> config/test_config.yaml
-echo "    R1: " $sample_name1_simple2 >> config/test_config.yaml
-echo "    R2: " $sample_name2_simple2 >> config/test_config.yaml
+echo "    R1: 'data/"$sample_name1_simple2"'" >> config/test_config.yaml
+echo "    R2: 'data/"$sample_name2_simple2"'" >> config/test_config.yaml
 done
 
 # Build the rest of the config file
 echo "
-extension:
-    .html: .html
-    _fastqc.zip: _fastqc.zip
-	
-trim5p:
-    10
-trimadapt:
-    True
-adapter1:
-    "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC"
-adapter2:
-    "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT"
-release:
-    104
+trim5p: 10
+trimAdapt: True
+adapter1: "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC"
+adapter2: "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT"
+release: 104
+threads: 6
 " >> config/test_config.yaml
 
 
