14e5649c094497f0f6204c30b867c2cc1417f2d4
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2021-10-14 16:53:39 -0700

    Split genome file download

--- scripts/STAR_index.sh
+++ scripts/STAR_index.sh
@@ -12,14 +12,24 @@ threads="$2"
 mkdir -p ref/release${release}/STARref
 mkdir -p ref/release${release}/STARindex
 
-# Download reference if empty
-if [ -z "$(ls -A ref/release${release}/STARref)" ]; then
+# Download gtf if not present
+gtf=ref/release${release}/STARref/Homo_sapiens.GRCh38.${release}.gtf
+
+if [ ! -e "$gtf" ]; then
     sudo curl -O --output-dir ref/release${release}/STARref ftp://ftp.ensembl.org/pub/release-${release}/gtf/homo_sapiens/Homo_sapiens.GRCh38.${release}.gtf.gz
-    sudo curl -O --output-dir ref/release${release}/STARref ftp://ftp.ensembl.org/pub/release-${release}/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
-    # Unzip
-    yes y | gunzip ref/release${release}/STARref/*
+    yes y | gunzip ref/release${release}/STARref/*gtf.gz
 else
-   echo "Files exist in ref/release#/STARref. No new files downloaded."
+    echo "Genome GTF already exists. No new file downloaded."
+fi
+
+# Download fasta if not present
+fasta=ref/release${release}/STARref/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+
+if [ ! -e "$fasta" ]; then
+    sudo curl -O --output-dir ref/release${release}/STARref ftp://ftp.ensembl.org/pub/release-${release}/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+    yes y | gunzip ref/release${release}/STARref/*fa.gz
+else
+    echo "Genome fasta already exists. No new file downloaded."
 fi
 
 # Make index if empty
