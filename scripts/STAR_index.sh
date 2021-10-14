426f885654d3662e57abe469fdb669e69a2e7ce4
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2021-10-14 11:10:18 -0700

    Add STAR index rule

---
+++ scripts/STAR_index.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#  STAR_index.sh
+#  Created by Kim Dill-McFarland on 10/14/21.
+#
+
+# Set vars
+release="$1"
+threads="$2"
+
+# Setup directories
+mkdir -p ref/release${release}/STARref
+mkdir -p ref/release${release}/STARindex
+
+# Make ftp path names
+gtf=ftp://ftp.ensembl.org/pub/release-${release}/gtf/homo_sapiens/Homo_sapiens.GRCh38.${release}.gtf.gz
+fasta=ftp://ftp.ensembl.org/pub/release-${release}/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
+
+# Download reference
+sudo curl -O --output-dir ref/release${release}/STARref ${gtf}
+sudo curl -O --output-dir ref/release${release}/STARref ${fasta}
+gunzip ref/release${release}/STARref/*
+
+# Index genome
+STAR --runMode genomeGenerate --genomeDir ref/release${release}/STARindex --genomeFastaFiles ref/release${release}/STARref/Homo_sapiens.GRCh38.dna.primary_assembly.fa --sjdbGTFfile ref/release${release}/STARref/Homo_sapiens.GRCh38.${release}.gtf --sjdbOverhang 99 --runThreadN ${threads}
