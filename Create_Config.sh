c3f169c19627bb7ed7cc2de9409f486dd3d931cf
rmsegnitz <43895313+rmsegnitz@users.noreply.github.com>
2021-10-13 17:05:01 -0700

    Update Create_Config.sh
    Update to new config format.

--- Create_Config.sh
+++ Create_Config.sh
@@ -8,7 +8,7 @@
 samples_list="data/samples/*R1*"
 
 
-echo "samples:" > config/test_config.yaml
+echo "sample_list:" > config/test_config.yaml
 
 spacer1=": "
 spacer2=" "
@@ -24,21 +24,30 @@ sample_name1_simple2=`echo "$sample_name1_simple" | cut -d'.' -f1`
 
 sample_name2_simple2=`echo "${sample_name1_simple2/R1/R2}"`
 
-sample_name_spaced="$indent$sample_name$spacer1$sample_name1_simple2$spacer2$sample_name2_simple2"
-
-echo $sample_name_spaced >> config/test_config.yaml
+# sample_name_spaced="$indent$sample_name$spacer1$sample_name1_simple2$spacer2$sample_name2_simple2"
+echo "  " "$sample_name$spacer1" >> config/test_config.yaml
+echo "    sample: " $sample_name >> config/test_config.yaml
+# echo $sample_name_spaced >> config/test_config.yaml
+echo "    R1: " $sample_name1_simple2 >> config/test_config.yaml
+echo "    R2: " $sample_name2_simple2 >> config/test_config.yaml
 done
 
 # Build the rest of the config file
-echo "extension:
+echo "
+extension:
     .html: .html
     _fastqc.zip: _fastqc.zip
+	
 trim5p:
     10
+trimadapt:
+    True
 adapter1:
     "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC"
 adapter2:
     "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT"
+release:
+    104
 " >> config/test_config.yaml
 
 
