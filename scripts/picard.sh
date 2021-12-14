7afbc4aa94fd3f29d8fd5ada7d4eb58cf79993b3
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2021-12-13 19:29:46 -0800

    Only download Picard ref if not already present in ref/

--- scripts/picard.sh
+++ scripts/picard.sh
@@ -8,13 +8,18 @@ input="$1"
 output="$2"
 
 # Download reference
-mkdir -p ref/PICARDref
-sudo curl -O --output-dir ref/PICARDref \
+if [ ref/PICARDref/refFlat.ensembl.txt ]; then
+  echo "Picard reference already present in ref/PICARDref."
+else
+  mkdir -p ref/PICARDref
+  sudo curl -O --output-dir ref/PICARDref \
     http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/refFlat.txt.gz
-gunzip ref/PICARDref/refFlat.txt.gz
+  gunzip ref/PICARDref/refFlat.txt.gz
 
-# Remove "chr" in chromosome name to match ensembl alignment using in STAR
-sed 's/chr//' ref/PICARDref/refFlat.txt > ref/PICARDref/refFlat.ensembl.txt
+  # Remove "chr" in chromosome name to match ensembl alignment using in STAR
+  sed 's/chr//' ref/PICARDref/refFlat.txt > ref/PICARDref/refFlat.ensembl.txt
+
+fi
 
 # Run Picard
  java -jar ~/apps/anaconda/share/picard-2.26.2-0/picard.jar \
