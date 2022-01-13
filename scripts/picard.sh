69f9d6a71b4cdb4e97aa4b4f81998fe297d0d0f1
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2022-01-13 14:46:06 -0800

    Revert "Update picard syntax"
    This reverts commit 498565b33a56b7602ade9f2350a60008f1820188.

--- scripts/picard.sh
+++ scripts/picard.sh
@@ -22,7 +22,7 @@ fi
 
 # Run Picard
 picard CollectRnaSeqMetrics \
-    -REF_FLAT ref/PICARDref/refFlat.ensembl.txt \
-    -INPUT ${input} -OUTPUT ${output} \
-    -ASSUME_SORTED true -STRAND_SPECIFICITY NONE -MINIMUM_LENGTH 500 \
-    -QUIET true -VERBOSITY ERROR
+    REF_FLAT=ref/PICARDref/refFlat.ensembl.txt \
+    INPUT=${input} OUTPUT=${output} \
+    ASSUME_SORTED=true STRAND_SPECIFICITY=NONE MINIMUM_LENGTH=500 \
+    QUIET=true VERBOSITY=ERROR
