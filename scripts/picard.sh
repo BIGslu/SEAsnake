7c6b58a1c079b2d403aa037d62ef58c31486bef0
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2022-04-15 10:20:35 -0700

    Add genome option to Picard

--- scripts/picard.sh
+++ scripts/picard.sh
@@ -6,6 +6,15 @@
 # Set vars
 input="$1"
 output="$2"
+genome="$3"
+
+if [ "$genome" == "Homo_sapiens.GRCh38" ]; then
+    genome2="hg38"
+elif [ "$genome" == "Mus_musculus.GRCm39" ]; then
+    genome2="mm39"
+else
+    echo "Picard option currently only suports hg38 and mm39."
+fi
 
 # Download reference
 if [ -f ref/PICARDref/refFlat.ensembl.txt ]; then
@@ -13,7 +22,7 @@ if [ -f ref/PICARDref/refFlat.ensembl.txt ]; then
 else
   mkdir -p ref/PICARDref
   sudo curl -O --output-dir ref/PICARDref \
-    http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/refFlat.txt.gz
+    http://hgdownload.cse.ucsc.edu/goldenPath/${genome2}/database/refFlat.txt.gz
   gunzip ref/PICARDref/refFlat.txt.gz
 
   # Remove "chr" in chromosome name to match ensembl alignment using in STAR
